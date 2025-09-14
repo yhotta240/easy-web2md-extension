73910d57085a60840c3eed22cdbd6d9356b66d2a
yhotta240 <yhotta240@icloud.com>
2025-09-14 14:48:23 +0900

    feat: メッセージ出力機能を追加

--- src/components/popupPanel.ts
+++ src/components/popupPanel.ts
@@ -1,148 +1,162 @@
 export class PopupPanel {
-    private header: HTMLElement;
-    private tabMenu: HTMLElement;
-    private maximizeButton: HTMLButtonElement;
-    private minimizeButton: HTMLButtonElement;
-    private closeButton: HTMLButtonElement;
-    private panelButton: HTMLButtonElement;
-    private resizer: HTMLElement;
-    private panel: HTMLElement;
-    private messagePanel: HTMLElement;
+  private header: HTMLElement;
+  private tabMenu: HTMLElement;
+  private maximizeButton: HTMLButtonElement;
+  private minimizeButton: HTMLButtonElement;
+  private closeButton: HTMLButtonElement;
+  private panelButton: HTMLButtonElement;
+  private resizer: HTMLElement;
+  private panel: HTMLElement;
+  private messagePanel: HTMLElement;
+  private messageDiv: HTMLElement;
 
-    private startY: number = 0;
-    private tmpPanelHeight: number = 0;
-    private startHeightTop: number = 0;
-    private emdHeight: number = 150;
-    private isDragging: boolean = false;
+  private startY: number = 0;
+  private tmpPanelHeight: number = 0;
+  private startHeightTop: number = 0;
+  private emdHeight: number = 150;
+  private isDragging: boolean = false;
 
-    // コンストラクタ
-    constructor() {
-        this.header = document.querySelector('#header')!;
-        this.tabMenu = document.querySelector('#tab-menu')!;
-        this.maximizeButton = document.querySelector("#maximize-button")!;
-        this.minimizeButton = document.querySelector("#minimize-button")!;
-        this.closeButton = document.querySelector("#close-button")!;
-        this.panelButton = document.querySelector("#panel-button")!;
-        this.resizer = document.getElementById('resizer')!;
-        this.panel = document.getElementById('panel')!;
-        this.messagePanel = document.querySelector('#messagePanel')!;
+  // コンストラクタ
+  constructor() {
+    this.header = document.querySelector('#header')!;
+    this.tabMenu = document.querySelector('#tab-menu')!;
+    this.maximizeButton = document.querySelector("#maximize-button")!;
+    this.minimizeButton = document.querySelector("#minimize-button")!;
+    this.closeButton = document.querySelector("#close-button")!;
+    this.panelButton = document.querySelector("#panel-button")!;
+    this.resizer = document.getElementById('resizer')!;
+    this.panel = document.getElementById('panel')!;
+    this.messagePanel = document.querySelector('#messagePanel')!;
+    this.messageDiv = document.getElementById('message')!;
 
-        this.initializePanel();
-        this.addEventListeners();
+    this.initializePanel();
+    this.addEventListeners();
+  }
+
+  private initializePanel(): void {
+    this.panel.style.display = 'block';
+    this.panel.style.height = '0';
+    this.closeButton.style.display = 'none';
+    this.switchMinMaxButtons();
+  }
+
+  private getPanelHeight(): number {
+    return document.documentElement.clientHeight - this.tabMenu.offsetHeight - this.resizer.offsetHeight;
+  }
+
+  private togglePanel(isOpen: boolean): void {
+    if (!this.isDragging) {
+      this.panel.style.height = isOpen ? `${this.emdHeight}px` : '0px';
     }
 
-    private initializePanel(): void {
-        this.panel.style.display = 'block';
-        this.panel.style.height = '0';
-        this.closeButton.style.display = 'none';
+    const panelHeight = parseFloat(this.panel.style.height);
+    const isPanelVisible = panelHeight > 50 && isOpen;
+
+    this.closeButton.style.display = isPanelVisible ? 'block' : 'none';
+    this.closeButton.children[0].setAttribute('style', `display: ${isPanelVisible ? 'block' : 'none'}`);
+    this.panelButton.style.display = isPanelVisible ? 'none' : 'block';
+    this.panelButton.children[0].setAttribute('style', `display: ${isPanelVisible ? 'none' : 'block'}`);
+
+    if (isOpen && (this.panel.offsetHeight === this.getPanelHeight())) {
+      this.maximizeButton.style.display = 'none';
+      this.minimizeButton.style.display = 'block';
+    }
+  }
+
+  private switchMinMaxButtons(): void {
+    const panelHeight = parseFloat(this.panel.style.height);
+    const isMaximized = panelHeight > this.getPanelHeight() - 20;
+    this.maximizeButton.style.display = isMaximized ? 'none' : 'block';
+    this.minimizeButton.style.display = isMaximized ? 'block' : 'none';
+  }
+
+  private addEventListeners(): void {
+    this.panelButton.addEventListener('click', () => {
+      this.togglePanel(true);
+      this.switchMinMaxButtons();
+    });
+
+    this.closeButton.addEventListener('click', () => {
+      this.togglePanel(false);
+      this.switchMinMaxButtons();
+      this.emdHeight = this.panel.offsetHeight > this.getPanelHeight() - 20 ? 150 : this.panel.offsetHeight;
+    });
+
+    this.resizer.addEventListener('mousedown', (e: MouseEvent) => {
+      this.isDragging = true;
+      this.panel.classList.add('no-transition');
+      this.resizer.style.backgroundColor = '#4688F1';
+      this.startY = e.clientY;
+      this.startHeightTop = this.panel.offsetHeight;
+      this.tmpPanelHeight = (this.panel.offsetHeight === 0 || this.panel.offsetHeight === this.getPanelHeight() || parseFloat(this.panel.style.height) > this.getPanelHeight() - 15) ? 150 : this.panel.offsetHeight;
+    });
+
+    window.addEventListener('mousemove', (e: MouseEvent) => {
+      if (this.isDragging) {
+        if (this.header.offsetHeight >= e.clientY - 20) {
+          this.panel.style.height = `${this.getPanelHeight()}px`;
+          return;
+        }
+        const dy = e.clientY - this.startY;
+        const newHeightTop = this.startHeightTop - dy;
+        this.panel.style.height = `${newHeightTop}px`;
+        this.togglePanel(true);
         this.switchMinMaxButtons();
-    }
+      }
+    });
 
-    private getPanelHeight(): number {
-        return document.documentElement.clientHeight - this.tabMenu.offsetHeight - this.resizer.offsetHeight;
-    }
-
-    private togglePanel(isOpen: boolean): void {
-        if (!this.isDragging) {
-            this.panel.style.height = isOpen ? `${this.emdHeight}px` : '0px';
+    window.addEventListener('mouseup', () => {
+      if (this.isDragging) {
+        this.isDragging = false;
+        this.panel.classList.remove('no-transition');
+        this.resizer.style.backgroundColor = '';
+        this.emdHeight = this.panel.offsetHeight;
+        const panelHeights = this.panel.offsetHeight;
+        if (panelHeights < 50) {
+          this.emdHeight = 150;
+          this.togglePanel(false);
         }
-
-        const panelHeight = parseFloat(this.panel.style.height);
-        const isPanelVisible = panelHeight > 50 && isOpen;
-
-        this.closeButton.style.display = isPanelVisible ? 'block' : 'none';
-        this.closeButton.children[0].setAttribute('style', `display: ${isPanelVisible ? 'block' : 'none'}`);
-        this.panelButton.style.display = isPanelVisible ? 'none' : 'block';
-        this.panelButton.children[0].setAttribute('style', `display: ${isPanelVisible ? 'none' : 'block'}`);
-
-        if (isOpen && (this.panel.offsetHeight === this.getPanelHeight())) {
-            this.maximizeButton.style.display = 'none';
-            this.minimizeButton.style.display = 'block';
+        if (panelHeights > this.getPanelHeight() - 20) {
+          this.emdHeight = 150;
+          this.panel.style.height = `${this.getPanelHeight()}px`;
         }
+        this.switchMinMaxButtons();
+      }
+    });
+
+    this.maximizeButton.addEventListener('click', () => {
+      this.tmpPanelHeight = (this.panel.offsetHeight === 0 || this.panel.offsetHeight === this.getPanelHeight() || parseFloat(this.panel.style.height) > this.getPanelHeight() - 20) ? 150 : this.panel.offsetHeight;
+      this.togglePanel(true);
+      this.panel.style.height = `${this.getPanelHeight()}px`;
+      this.switchMinMaxButtons();
+    });
+
+    this.minimizeButton.addEventListener('click', () => {
+      this.togglePanel(true);
+      this.panel.style.height = `${this.tmpPanelHeight}px`;
+      this.switchMinMaxButtons();
+    });
+
+    window.addEventListener('resize', () => {
+      const currentHeight = parseFloat(this.panel.style.height);
+      const maxHeight = this.getPanelHeight();
+      this.panel.style.height = `${Math.min(currentHeight, maxHeight)}px`;
+      if (this.minimizeButton.style.display === 'block') {
+        this.panel.style.height = `${maxHeight}px`;
+      }
+    });
+  }
+
+  /**
+   * メッセージを出力
+   * @param datetime 現在日時
+   * @param message メッセージ
+   * @returns
+   */
+  public messageOutput(datetime: string, message: string): void {
+    if (this.messageDiv) {
+      this.messageDiv.innerHTML += `<p class="m-0">${datetime} ${message}</p>`;
     }
-
-    private switchMinMaxButtons(): void {
-        const panelHeight = parseFloat(this.panel.style.height);
-        const isMaximized = panelHeight > this.getPanelHeight() - 20;
-        this.maximizeButton.style.display = isMaximized ? 'none' : 'block';
-        this.minimizeButton.style.display = isMaximized ? 'block' : 'none';
-    }
-
-    private addEventListeners(): void {
-        this.panelButton.addEventListener('click', () => {
-            this.togglePanel(true);
-            this.switchMinMaxButtons();
-        });
-
-        this.closeButton.addEventListener('click', () => {
-            this.togglePanel(false);
-            this.switchMinMaxButtons();
-            this.emdHeight = this.panel.offsetHeight > this.getPanelHeight() - 20 ? 150 : this.panel.offsetHeight;
-        });
-
-        this.resizer.addEventListener('mousedown', (e: MouseEvent) => {
-            this.isDragging = true;
-            this.panel.classList.add('no-transition');
-            this.resizer.style.backgroundColor = '#4688F1';
-            this.startY = e.clientY;
-            this.startHeightTop = this.panel.offsetHeight;
-            this.tmpPanelHeight = (this.panel.offsetHeight === 0 || this.panel.offsetHeight === this.getPanelHeight() || parseFloat(this.panel.style.height) > this.getPanelHeight() - 15) ? 150 : this.panel.offsetHeight;
-        });
-
-        window.addEventListener('mousemove', (e: MouseEvent) => {
-            if (this.isDragging) {
-                if (this.header.offsetHeight >= e.clientY - 20) {
-                    this.panel.style.height = `${this.getPanelHeight()}px`;
-                    return;
-                }
-                const dy = e.clientY - this.startY;
-                const newHeightTop = this.startHeightTop - dy;
-                this.panel.style.height = `${newHeightTop}px`;
-                this.togglePanel(true);
-                this.switchMinMaxButtons();
-            }
-        });
-
-        window.addEventListener('mouseup', () => {
-            if (this.isDragging) {
-                this.isDragging = false;
-                this.panel.classList.remove('no-transition');
-                this.resizer.style.backgroundColor = '';
-                this.emdHeight = this.panel.offsetHeight;
-                const panelHeights = this.panel.offsetHeight;
-                if (panelHeights < 50) {
-                    this.emdHeight = 150;
-                    this.togglePanel(false);
-                }
-                if (panelHeights > this.getPanelHeight() - 20) {
-                    this.emdHeight = 150;
-                    this.panel.style.height = `${this.getPanelHeight()}px`;
-                }
-                this.switchMinMaxButtons();
-            }
-        });
-
-        this.maximizeButton.addEventListener('click', () => {
-            this.tmpPanelHeight = (this.panel.offsetHeight === 0 || this.panel.offsetHeight === this.getPanelHeight() || parseFloat(this.panel.style.height) > this.getPanelHeight() - 20) ? 150 : this.panel.offsetHeight;
-            this.togglePanel(true);
-            this.panel.style.height = `${this.getPanelHeight()}px`;
-            this.switchMinMaxButtons();
-        });
-
-        this.minimizeButton.addEventListener('click', () => {
-            this.togglePanel(true);
-            this.panel.style.height = `${this.tmpPanelHeight}px`;
-            this.switchMinMaxButtons();
-        });
-
-        window.addEventListener('resize', () => {
-            const currentHeight = parseFloat(this.panel.style.height);
-            const maxHeight = this.getPanelHeight();
-            this.panel.style.height = `${Math.min(currentHeight, maxHeight)}px`;
-            if (this.minimizeButton.style.display === 'block') {
-                this.panel.style.height = `${maxHeight}px`;
-            }
-        });
-    }
+  }
 }
 
